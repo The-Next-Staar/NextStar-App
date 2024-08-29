be1d41b0feb166afdb8726c56ac1b1c0c6c7fbcb
Junhyeong Park <pljh0906@yonsei.ac.kr>
2024-08-29 12:03:59 +0900

    feat: <individuals> subscription on process

--- lib/pages/individuals/individuals_mainpage/management_subscription_page.dart
+++ lib/pages/individuals/individuals_mainpage/management_subscription_page.dart
@@ -61,7 +61,7 @@ class SubscriptionPage extends StatelessWidget {
               const SizedBox(height: 15),
               _buildFeatureItem('3', '오디션 우선 제공', '비공개 오디션 기회를 누구보다 먼저 누리세요!'),
               const Spacer(),
-              _buildSubscriptionButton(),
+              _buildSubscriptionButton(context),
               const SizedBox(height: 20),
             ],
           ),
@@ -130,50 +130,98 @@ class SubscriptionPage extends StatelessWidget {
     );
   }
 
-  Widget _buildSubscriptionButton() {
-    return Container(
-      width: 350,
-      height: 70,
-      decoration: BoxDecoration(
-        color: const Color(0xFFEF69A6),
-        borderRadius: BorderRadius.circular(6),
-      ),
-      child: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                '월 26,000원',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 12,
-                  fontWeight: FontWeight.w400,
-                  decoration: TextDecoration.lineThrough,
+  Widget _buildSubscriptionButton(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              backgroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              title: const Center(
+                child: Text(
+                  '준비 중입니다.',
+                  style: TextStyle(
+                    color: Color(0xFFEF69A6),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
               ),
-              SizedBox(width: 10),
-              Text(
-                '7일 무료체험',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 12,
-                  fontWeight: FontWeight.w600,
+              actions: <Widget>[
+                Center(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(); // 팝업 닫기
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: const Color(0xFFEF69A6),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                    ),
+                    child: const Text(
+                      '확인',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(height: 5),
-          Text(
-            '나만의 매니저 고용하기',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-              fontWeight: FontWeight.w700,
+              ],
+            );
+          },
+        );
+      },
+      child: Container(
+        width: 350,
+        height: 70,
+        decoration: BoxDecoration(
+          color: const Color(0xFFEF69A6),
+          borderRadius: BorderRadius.circular(6),
+        ),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '월 26,000원',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    decoration: TextDecoration.lineThrough,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Text(
+                  '7일 무료체험',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+            SizedBox(height: 5),
+            Text(
+              '나만의 매니저 고용하기',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
